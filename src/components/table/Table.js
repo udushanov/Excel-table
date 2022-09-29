a71deaed38834713f7e266f1eb4750b2643d45b4
Ulugbek <u.dushanov@gmail.com>
2022-09-29 21:40:16 +0500

    feat: add some functionality

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -36,6 +36,10 @@ export class Table extends ExcelComponent {
     this.$on('formula:done', () => {
       this.selection.current.focus();
     })
+
+    this.$subscribe((state) => {
+      console.log(state)
+    })
   }
 
   selectCell($cell) {
@@ -54,7 +58,7 @@ export class Table extends ExcelComponent {
             .map((id) => this.$root.find(`[data-id="${id}"]`));
         this.selection.selectGroup($cells);
       } else {
-        this.selection.select($target);
+        this.selectCell($target);
       }
     }
   }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import {Formula} from './components/formula/Formula';
 import {Header} from './components/header/Header';
 import {Table} from './components/table/Table';
 import {Toolbar} from './components/toolbar/Toolbar';
+import {createStore} from './core/createStore';
+import {rootReducer} from './components/redux/rootReducer';
+
 import './scss/index.scss';
 
+const store = createStore(rootReducer, {
+  tableTitle: 'my table'
+});
+
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 });
 
 excel.render();
